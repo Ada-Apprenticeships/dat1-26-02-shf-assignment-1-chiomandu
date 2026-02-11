f8c026d745455e5cafe9c4b99cb96b7adf0f9f36
chiomandu <chioma.ndu@ada.ac.uk>
2026-02-11 11:54:04 +0000

    changes

--- src/queries/1_user_management.sql
+++ src/queries/1_user_management.sql
@@ -2,15 +2,24 @@
 .mode column
 
 -- 1.1
-
+SELECT * FROM members
 
 -- 1.2
-
+UPDATE members
+SET
+phone_number='07000 100005',
+email='emily.jones.updated@email.com'
+WHERE member_id '5';
 
 -- 1.3
-
+SELECT COUNT * AS total_members
+FROM members;
 
 -- 1.4
+SELECT 
+m.member_id, m.first_name, m.last_name,
+COUNT(ca.class_attendance_id) AS registration_count FROM member AS m 
+LEFT JOIN class_attendance AS ca 
 
 
 -- 1.5
